c3a30e047d2034f82a77226a29b25da6b9457559
Vladyslav Savkiv <sav320801@gmail.com>
2026-04-04 10:01:52 +0100

    Add: jump-game-ii - submission-10

---
+++ Data Structures & Algorithms/jump-game-ii/submission-10.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length == 0 || nums.length == 1) {
+            return 0;
+        }
+        int currentEnd = 0;
+
+        int jumps = 0;
+        int farthest = 0;
+
+        for(int i = 0; i < nums.length-1; i++) {
+            
+            farthest = Math.max(farthest, nums[i] + i);
+
+            if(i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+            
+        
+        }
+        if(jumps == 0) {
+            return 1;
+        }
+        return jumps;
+    }
+} 
