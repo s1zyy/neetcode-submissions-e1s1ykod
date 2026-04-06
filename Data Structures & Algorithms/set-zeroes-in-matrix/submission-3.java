d80e33444905693f493e3b29b55dd18671009f46
Vladyslav Savkiv <sav320801@gmail.com>
2026-04-06 16:19:07 +0100

    Add: set-zeroes-in-matrix - submission-3

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-3.java
@@ -0,0 +1,55 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean top = false;
+        boolean left = false;
+
+        for(int i = 0; i < matrix[0].length; i++) {
+            if(matrix[0][i] == 0) {
+                top = true;
+            }
+        }
+        for(int i = 0; i < matrix.length; i++) {
+            if(matrix[i][0] == 0) {
+                left = true;
+            }
+        }
+
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < matrix.length; i++) {
+            if(matrix[i][0] == 0) {
+                for(int k = 0; k < matrix[0].length; k++) {
+                    matrix[i][k] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < matrix[0].length; i++) {
+            if(matrix[0][i] == 0) {
+                for(int k = 0; k < matrix.length; k++) {
+                    matrix[k][i] = 0;
+                }
+            }
+        }
+
+        if(top) {
+            for(int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(left) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        
+    }
+}
