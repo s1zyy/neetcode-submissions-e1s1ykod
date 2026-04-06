e9e556d26979449131034851d2e3f3362023994c
Vladyslav Savkiv <sav320801@gmail.com>
2026-04-06 09:17:22 +0100

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.java
@@ -0,0 +1,58 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        int size = intervals.size();
+        if(size==0) {
+            return 0;
+        } else if(size ==1) {
+            return 1;
+        }
+        int counter = 0;
+
+        int[] starts = new int[size];
+        int[] ends = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            Interval interval = intervals.get(i);
+            starts[i] = interval.start;
+            ends[i] = interval.end;
+        }
+
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int max = 0;
+
+        int s = 0; 
+        int e = 0;
+       
+
+        while(s < size) {
+            
+            if(starts[s] < ends[e]) {
+                counter++;
+                s++;
+            } else if( starts[s] >= ends[e]){
+                counter--;
+                e++;
+            }
+
+            max = Math.max(max, counter);
+        
+        }
+
+        return max;
+
+
+
+    }
+}
