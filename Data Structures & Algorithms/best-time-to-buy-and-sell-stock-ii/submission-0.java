d8af888bc295d0b67241fbd1f276aaa049c96109
Vladyslav Savkiv <sav320801@gmail.com>
2026-08-26 13:08:36 +0100

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if(len == 1) return 0;
+        int profit = 0;
+        for(int i = 1; i < len; i++) {
+            if(prices[i] > prices[i-1]) {
+                profit += (prices[i]-prices[i-1]);
+            }
+        }
+        return profit;
+        
+    }
+}
